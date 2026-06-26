bc0ff4677433cc750ff18707004e3a8fc9c80949
palakgupt4 <palakgupt42@gmail.com>
2026-06-26 16:03:09 +0530

    Add files via upload

---
+++ 01_Basic_Gates/not_gate.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.06.2026 17:43:09
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module not_gate(
+input a,
+output y
+    );
+    assign y = ~a;
+endmodule
---
+++ 01_Basic_Gates/xor_gate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.06.2026 17:43:09
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module xor_gate(
+input a,
+input b,
+output y
+    );
+    assign y = a ^ b;
+endmodule
